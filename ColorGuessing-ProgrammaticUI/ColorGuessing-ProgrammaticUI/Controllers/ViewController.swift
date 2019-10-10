24a06ad6945b54580528d8095f990d60fa35974f
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-10 16:39:02 -0400

    adds UI lazy variables

--- ColorGuessing-ProgrammaticUI/ColorGuessing-ProgrammaticUI/Controllers/ViewController.swift
+++ ColorGuessing-ProgrammaticUI/ColorGuessing-ProgrammaticUI/Controllers/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Lazy Variables
+       lazy var highScoreLabel: UILabel = {
+           let label = UILabel()
+           return label
+       }()
+       
+       lazy var currentScoreLabel: UILabel = {
+           let label = UILabel()
+           return label
+       }()
+       
+       lazy var colorBoard: UIView = {
+           let colorboard = UIView()
+           return colorboard
+       }()
+       
+       lazy var gameOverLabel: UILabel = {
+           let label = UILabel()
+           label.textAlignment = .center
+           return label
+       }()
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
